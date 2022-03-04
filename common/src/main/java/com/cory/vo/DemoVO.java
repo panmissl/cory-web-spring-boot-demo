b66a5da48f36477527b399c546c6cbb6829c1dcd
panliang <panmissl@163.com>
2022-03-04 22:36:28 +0800

    update

--- common/src/main/java/com/cory/vo/DemoVO.java
+++ common/src/main/java/com/cory/vo/DemoVO.java
@@ -1,6 +1,9 @@
 package com.cory.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -9,6 +12,9 @@ import java.util.Date;
  * Created by Cory on 2021/2/17.
  */
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DemoVO implements Serializable {
 
     private String name;
