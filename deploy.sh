96564780c019030fa57d9449ce52afc08b90e6b6
panliang <panmissl@163.com>
2022-04-21 06:44:13 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 # 上传
 # 执行服务器脚本
 
-mvn -DskipTests=true -Dmaven.skip.test=true package
+mvn -DskipTests=true -Dmaven.skip.test=true clean package
 scp /Users/corypan/Documents/panliang/idea_workspace/miquan/web/target/web-0.0.1-SNAPSHOT.jar username@server:/xxx/cory-web/web-0.0.1-SNAPSHOT.jar.new
 #本地运行远程脚本会导致进程杀不死，所以提示去远程执行
 #ssh username@server "/xxx/cory-web/deploy.sh"
